102a12fbf643618f8b3b1c84e87f18b00a2af61a
Amirkhan <email@example.com>
2026-01-22 20:00:45 +0500

    feat(utils):add database connection

---
+++ src/utils/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:335432/card_battle_db";
+    private static final String user = "postgres";
+    private static final String password = "18112007Amir";
+
+    public static Connection getConnection() throws SQLException {
+        try{
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL JDBC Driver not found", e);
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn !=null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
